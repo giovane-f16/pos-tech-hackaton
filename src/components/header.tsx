41254ddeb2c48c86672d8d17884ebff9b5a05b48
giovanef16 <giovane_05silva@hotmail.com>
2026-02-03 09:15:38 -0300

    Ajustando header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -29,22 +29,24 @@ const Header = () => {
     },[value]);
 
     return (
-        <header className="w-full bg-blue-200 dark:bg-gray-800 p-2 flex items-center justify-between">
-            <div className="flex-row items-center gap-2">
-                <a href="/" className="flex items-center gap-2">
-                    <GraduationCap className="size-12 text-blue-600" />
-                    <h1 className="text-2xl font-bold text-start text-gray-900 dark:text-gray-100">
-                        Aprendia
-                    </h1>
-                </a>
-            </div>
-            <div className="flex gap-x-6">
-                <User />
-                <label className="inline-flex items-center cursor-pointer">
-                    <input type="checkbox" checked={value} onChange={(e) => setValue(e.target.checked)} className="sr-only peer" onClick={handleActiveDarkMode}/>
-                    <div className="relative w-9 h-5 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-600"></div>
-                    <span className="select-none ms-3 text-sm font-medium text-gray-900 dark:text-gray-100">Dark Mode</span>
-                </label>
+        <header className="w-full bg-white-200 dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
+            <div className="p-2 flex items-center justify-between container mx-auto">
+                <div className="flex-row items-center gap-2">
+                    <a href="/" className="flex items-center gap-2">
+                        <GraduationCap className="size-12 text-blue-600" />
+                        <h1 className="text-2xl font-bold text-start text-gray-900 dark:text-gray-100">
+                            Aprendia
+                        </h1>
+                    </a>
+                </div>
+                <div className="flex gap-x-6">
+                    <User />
+                    <label className="inline-flex items-center cursor-pointer">
+                        <input type="checkbox" checked={value} onChange={(e) => setValue(e.target.checked)} className="sr-only peer" onClick={handleActiveDarkMode}/>
+                        <div className="relative w-9 h-5 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-600"></div>
+                        <span className="select-none ms-3 text-sm font-medium text-gray-900 dark:text-gray-100">Dark Mode</span>
+                    </label>
+                </div>
             </div>
         </header>
     );
