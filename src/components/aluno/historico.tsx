6c405034c5917ec6b842118f05cc685a5f9c6a23
giovanef16 <giovane_05silva@hotmail.com>
2026-02-08 13:21:37 -0300

    Removendo botão

--- src/components/aluno/historico.tsx
+++ src/components/aluno/historico.tsx
@@ -52,9 +52,6 @@ const Historico = () => {
                             <li key={entrega._id} className="border border-gray-200 dark:border-gray-700 p-4 rounded-lg mb-4 flex flex-col gap-2 dark:bg-gray-900">
                                 <div className="flex items-center justify-between">
                                     <h3 className="font-medium text-[18px] dark:text-white">{entrega.trabalhoTitulo}</h3>
-                                    <button className="border border-gray-200 dark:border-gray-600 hover:border-gray-400 dark:hover:border-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700 rounded px-4 py-1 cursor-pointer flex items-center justify-center gap-2 font-medium text-[12px] dark:text-gray-200">
-                                        Ver Detalhes
-                                    </button>
                                 </div>
                                 <p className="text-sm text-gray-700 dark:text-gray-300">Título: {entrega.titulo}</p>
                                 <p className="text-sm text-gray-600 dark:text-gray-400">Enviado em: {formatarData(entrega.dataRecebimento)}</p>
