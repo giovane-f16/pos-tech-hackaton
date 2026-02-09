c1eed3191995af2f2b762ee4485d814fa4ed8723
giovanef16 <giovane_05silva@hotmail.com>
2026-02-09 10:51:15 -0300

    Diferenciando Header

--- src/components/header-wrapper.tsx
+++ src/components/header-wrapper.tsx
@@ -5,7 +5,8 @@ import { getTheme } from "@/providers/theme";
 export default async function HeaderWrapper() {
     const authProvider = new AuthProvider();
     const nome = await authProvider.getNome();
+    const tipoUsuario = await authProvider.getTipoUsuario();
     const theme = await getTheme();
 
-    return <Header nome={nome} theme={theme} />;
+    return <Header nome={nome} tipoUsuario={tipoUsuario} theme={theme} />;
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,18 +6,22 @@ import ThemeToggle from "@/components/theme-toggle";
 
 interface HeaderProps {
     nome: string | null;
+    tipoUsuario: string | null;
     theme: string;
 }
 
-const Header = ({ nome, theme }: HeaderProps) => {
+const Header = ({ nome, tipoUsuario, theme }: HeaderProps) => {
     return (
         <header className="w-full bg-white-200 dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
             <div className="p-2 flex items-center justify-between container mx-auto">
                 <div className="flex-row items-center gap-2">
                     <a href="/" className="flex items-center gap-2">
-                        <GraduationCap className="size-12 text-blue-600" />
+                        <GraduationCap className={`size-12 ${tipoUsuario === "professor" ? "text-green-600" : "text-blue-600"}`} />
                         <h1 className="text-2xl font-bold text-start text-gray-900 dark:text-gray-100">
                             Aprendia
+                            {tipoUsuario && (
+                                <p className="text-[12px] font-normal">Área do <span className="capitalize">{tipoUsuario}</span></p>
+                            )}
                         </h1>
                     </a>
                 </div>
