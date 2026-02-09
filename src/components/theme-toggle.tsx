820c3ec4d26555e148f4b304b98f6dc596d27ecc
giovanef16 <giovane_05silva@hotmail.com>
2026-02-09 11:29:46 -0300

    Ajustando header no mobile

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -38,7 +38,7 @@ export default function ThemeToggle({ initialTheme }: ThemeToggleProps) {
                 disabled={isPending}
             />
             <div className="relative w-9 h-5 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-600"></div>
-            <span className="select-none ms-3 text-sm font-medium text-gray-900 dark:text-gray-100">Dark Mode</span>
+            <span className="select-none ms-3 text-sm font-medium text-gray-900 dark:text-gray-100 hidden md:flex">Dark Mode</span>
         </label>
     );
 }
--- src/components/user.tsx
+++ src/components/user.tsx
@@ -18,8 +18,8 @@ export default function User({ nome }: UserClientProps) {
     }
 
     return (
-        <div className="flex items-center gap-4">
-            <p className="text-gray-900 dark:text-gray-100">Bem vindo, {nome}!</p>
+        <div className="md:flex items-center gap-4">
+            <p className="text-gray-900 dark:text-gray-100 hidden md:flex">Bem vindo, {nome}!</p>
             <button className="px-4 py-1 bg-gray-50 rounded hover:bg-gray-100 text-gray-900 dark:bg-gray-600 dark:text-gray-100 dark:hover:bg-gray-500 cursor-pointer flex items-center gap-2 border border-gray-200" onClick={logout}>
                 <LogOut size={16} />
                 Sair
